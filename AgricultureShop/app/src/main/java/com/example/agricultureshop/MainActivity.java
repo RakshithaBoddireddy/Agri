5d7a32cac00f49e71285d98e642c95aa13777eb9
RakshithaBoddireddy <S564570@nwmissouri.edu>
2024-03-04 16:46:19 -0600

    Added code for main activity(Rakshitha Boddireddy)
    Added Onclick for buttons

--- AgricultureShop/app/src/main/java/com/example/agricultureshop/MainActivity.java
+++ AgricultureShop/app/src/main/java/com/example/agricultureshop/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.agricultureshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button farmerBTN = findViewById(R.id.FarmerNavigateBTN);
+        Button userBTN = findViewById(R.id.UserNavigateBTN);
+
+        farmerBTN.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent ini1 = new Intent(MainActivity.this, FarmerLoginActivity.class);
+                startActivity(ini1);
+            }
+        });
+
+        userBTN.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent ini2 = new Intent(MainActivity.this, UserLoginActivity.class);
+                startActivity(ini2);
+            }
+        });
     }
 }
