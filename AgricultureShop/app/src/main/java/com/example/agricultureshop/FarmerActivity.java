a565d8ec34826d5469a42090d6768314fa8fbf2d
SaiprakashGoud <143534112+SaiprakashGoud@users.noreply.github.com>
2024-03-04 21:25:18 -0600

    farmer activity(Sai Prakash Goud)

---
+++ AgricultureShop/app/src/main/java/com/example/agricultureshop/FarmerActivity.java
@@ -0,0 +1,14 @@
+package com.example.agricultureshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FarmerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_farmer);
+    }
+}
