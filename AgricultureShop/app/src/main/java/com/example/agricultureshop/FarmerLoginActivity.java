7c3cecc9f8d540dfcbe241965fd9b4da261718d6
SaiprakashGoud <143534112+SaiprakashGoud@users.noreply.github.com>
2024-03-04 22:17:09 -0600

    filename corrected(Sai Prakash Goud)

--- AgricultureShop/app/src/main/java/com/example/agricultureshop/FarmerLoginActivity.java
+++ AgricultureShop/app/src/main/java/com/example/agricultureshop/FarmerLoginActivity.java
@@ -2,9 +2,6 @@ package com.example.agricultureshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
