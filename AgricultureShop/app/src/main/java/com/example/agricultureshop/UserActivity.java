7ae310f7fe83a9167d469c23459334866ce0b8b6
ShivaRamReddyMarthala <142836346+ShivaRamReddyMarthala@users.noreply.github.com>
2024-03-04 22:08:52 -0600

    Added user activity

---
+++ AgricultureShop/app/src/main/java/com/example/agricultureshop/UserActivity.java
@@ -0,0 +1,20 @@
+package com.example.agrishop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+
+
+    }
+}
