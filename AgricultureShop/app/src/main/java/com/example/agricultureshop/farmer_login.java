d33ef704aa2b90df8909dcf76f5181f756266aa3
SaiprakashGoud <143534112+SaiprakashGoud@users.noreply.github.com>
2024-03-04 18:38:30 -0600

    farmer login activity(Sai Prakash Goud)
    added onclick button with functionality

--- AgricultureShop/app/src/main/java/com/example/agricultureshop/farmer_login.java
+++ AgricultureShop/app/src/main/java/com/example/agricultureshop/farmer_login.java
@@ -4,11 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class farmer_login extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class FarmerLoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_farmer_login);
+
+        TextView farmer = findViewById(R.id.farmerTXT);
+        EditText pwd = findViewById(R.id.farmerET);
+
+
+        Button login = findViewById(R.id.farmerLoginBTN);
+        login.setOnClickListener(new View.OnClickListener()
+        {
+
+            public void onClick(View v) {
+                String farmerInStr=farmer.getText().toString();
+                String pwdInStr = pwd.getText().toString();
+                if (farmerInStr.equals(("android")) && pwdInStr.equals("project")) {
+                    Intent ini = new Intent(FarmerLoginActivity.this, FarmerActivity.class);
+                    startActivity(ini);
+                    finish();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Enter valid credentials", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
+
 }
